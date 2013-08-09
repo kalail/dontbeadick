05dbbcc5ee19aae8bdea398ad9e400e933a2737d
Kalail <kashif610@gmail.com>
2013-08-09 03:59:06 -0500

    Update hosts

--- dontbeadick/dontbeadick/settings.py
+++ dontbeadick/dontbeadick/settings.py
@@ -4,7 +4,7 @@ from unipath import Path
 PROJECT_DIR = Path(__file__).ancestor(2)
 get_path = lambda name: '{}/{}'.format(PROJECT_DIR, name)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -27,7 +27,11 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '96.126.122.107',
+    'www.dudedontbeadick.com'
+]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
